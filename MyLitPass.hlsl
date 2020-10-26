55f45e303f9954ba96f2f511ea79176f930e6149
pixar2016 <360970742@qq.com>
2020-10-26 23:17:22 +0800

    fix bugs

--- MyLitPass.hlsl
+++ MyLitPass.hlsl
@@ -3,8 +3,9 @@
 
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
 #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/UnityInstancing.hlsl"
-#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
 #include "Common.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
+
 
 TEXTURE2D(_BaseMap);
 SAMPLER(sampler_BaseMap);
@@ -18,24 +19,36 @@ struct Attributes{
     float3 positionOS : POSITION;
     float3 normalOS : NORMAL;
     float2 baseUV : TEXCOORD0;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
+    //UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings{
     float4 positionCS : SV_POSITION;
-    UNITY_VERTEX_INPUT_INSTANCE_ID
+    float3 positionWS : VAR_POSITION;
+    float2 baseUV : VAR_BASE_UV;
+    //UNITY_VERTEX_INPUT_INSTANCE_ID
 };
+
+#define INPUT_PROP(name) UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, name)
+
+float2 TransformBaseUV(float2 baseUV)
+{
+    float4 baseST = INPUT_PROP(_BaseMap_ST);
+    return baseUV * baseST.xy + baseST.zw;
+}
 
 Varyings LitPassVertex(Attributes input){
     Varyings output;
-    UNITY_SETUP_INSTANCE_ID(input);
-    UNITY_TRANSFER_INSTANCE_ID(input, output);
-    output.positionCS = TransformObjectToWorld(input.positionOS);
+    //UNITY_SETUP_INSTANCE_ID(input);
+    //UNITY_TRANSFER_INSTANCE_ID(input, output);
+    output.positionWS = TransformObjectToWorld(input.positionOS);
+    output.positionCS = TransformWorldToHClip(output.positionWS);
+    output.baseUV = TransformBaseUV(input.baseUV);
     return output;
 }
 
 float4 LitPassFragment(Varyings input):SV_TARGET{
-    UNITY_SETUP_INSTANCE_ID(input);
+    //UNITY_SETUP_INSTANCE_ID(input);
     float4 baseMap = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.baseUV);
     float4 baseColor = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
     float4 color = baseMap * baseColor;
--- MyLitPass.shader
+++ MyLitPass.shader
@@ -1,20 +1,22 @@
-Shader "PixarRenderPipeline/Lit"{
-    _Properties{
+Shader "PixarRenderPipeline/MyLit"{
+    Properties{
         _BaseMap("Texture", 2D) = "white"{}
         _BaseColor("Color", Color) = (0.5, 0.5, 0.5, 1.0)
     }
 
     SubShader{
+        Tags { "RenderType" = "Opaque" }
         Pass{
-            Tags{
-                "LightMode" = "CustomLit"
-            }
-
+            ZWrite On
             HLSLPROGRAM
-            #include "MyLitPass.hlsl"
+            #pragma target 3.5
+            //#pragma multi_compile_instancing
+            #pragma enable_d3d11_debug_symbols
             #pragma vertex LitPassVertex
             #pragma fragment LitPassFragment
+            #include "MyLitPass.hlsl"
             ENDHLSL
         }
     }
+    CustomEditor "LitShaderGUI"
 }
